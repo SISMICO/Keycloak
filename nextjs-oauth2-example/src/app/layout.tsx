1408aedf6b2659b26df511c6d220e354063c981e
Leonardo Bento <sismico@gmail.com>
2024-04-06 20:38:30 -0300

    Add Dockerfile for nextjs

--- nextjs-oauth2-example/src/app/layout.tsx
+++ nextjs-oauth2-example/src/app/layout.tsx
@@ -1,26 +1,19 @@
-'use client'
-
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
-type Props = {
-  children?: React.ReactNode;
-  session?: AppProps;
-};
-
-export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default function RootLayout({ children, session }: Props) {
+export default function RootLayout({ children }: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <Suspense>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
